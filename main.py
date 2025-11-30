904e01d86c40947205de567a8d624a4bb0a7d9d3
pipi2023 <peiying2023@163.com>
2025-11-30 14:27:42 +0800

    initial

---
+++ main.py
@@ -0,0 +1,11 @@
+import sys
+import os
+
+sys.path.append(os.path.join(os.path.dirname(__file__), 'src'))
+
+def main():
+    print("网络嗅探器启动...")
+    # TODO: 后续在这里添加主逻辑
+
+if __name__ == "__main__":
+    main()
